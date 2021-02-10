e4276d0045d61e97ac86d334b1d889b5b352708b
Darayavaus Grayson <dgrayson100@gmail.com>
2021-02-10 02:34:10 -0500

    Switched from P5 to standard canvas, got initial shapes to appear

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ var vH = 1;
 var O = [0,0,0]; 
 var d = 1; 
 
-var spheres = []; 
-
 class Sphere{
     constructor(center, radius, color){
         this.center = center; 
@@ -17,36 +15,58 @@ class Sphere{
     }
 }
 
-function setup(){
-
-    let s1 = new Sphere([30, 40], 5, red);
-    spheres = {
-        s1
+class Light{
+    constructor(type, intensity, position, direction){
+        this.type = type; 
+        this.intensity = intensity; 
+        this.position = position; 
+        this.direction = direction; 
     }
-    createCanvas(cW, cH);
 }
 
-function draw(){
-    let x; 
+var spheres = 
+    [new Sphere([0,1,3], 1, [255,0,0]),
+     new Sphere([1,0,2], .5, [150,130,200]), 
+     new Sphere([-.5,-.5,2], .5, [100,255,100])]; 
 
-    for(x = -cW/2; x < cW / 2; x++){
-        for(y = -cH / 2; y < cH / 2; y++ ){
-            let D = canvasToViewport(x, y); 
-            let color = TraceRay(O, D, 1, 0); 
-            set(x, y, color(100,50,100));  
-        }
-    }
 
-    console.log("Running"); 
-    set(50, 50, color(100, 50, 100));
-    updatePixels(); 
+// ======================================================================
+//  Low-level canvas access.
+// ======================================================================
 
-    background(0); 
+var canvas = document.getElementById("canvas");
+var canvas_context = canvas.getContext("2d");
+var canvas_buffer = canvas_context.getImageData(0, 0, canvas.width, canvas.height);
+var canvas_pitch = canvas_buffer.width * 4;
 
+
+// The PutPixel() function.
+var PutPixel = function(x, y, color) {
+  x = canvas.width/2 + x;
+  y = canvas.height/2 - y - 1;
+
+  if (x < 0 || x >= canvas.width || y < 0 || y >= canvas.height) {
+    return;
+  }
+
+  var offset = 4*x + canvas_pitch*y;
+  canvas_buffer.data[offset++] = color[0];
+  canvas_buffer.data[offset++] = color[1];
+  canvas_buffer.data[offset++] = color[2];
+  canvas_buffer.data[offset++] = 255; // Alpha = 255 (full opacity)
 }
 
-function getColor(){
+// Displays the contents of the offscreen buffer into the canvas.
+var UpdateCanvas = function() {
+    canvas_context.putImageData(canvas_buffer, 0, 0);
+  }
 
+function dot(v, w){
+    return v[0] * w[0] + v[1] * w[1] + v[2] * w[2]; 
+}
+
+function subtract(v1, v2){
+    return [v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2]]; 
 }
 
 function canvasToViewport(x, y){
@@ -54,7 +74,7 @@ function canvasToViewport(x, y){
 }
 
 function TraceRay(O, D, t_min, t_max){
-    let closest_t = null; 
+    let closest_t = Infinity; 
     let closest_sphere = null; 
 
     for(let x = 0; x < spheres.length; x++){
@@ -72,7 +92,7 @@ function TraceRay(O, D, t_min, t_max){
     }
 
     if (closest_sphere == null){
-        return color.black; 
+        return [0,0,0]; 
     }
     
     return closest_sphere.color; 
@@ -94,10 +114,15 @@ function IntersectRaySphere(O, D, sphere){
     return [t1, t2]; 
 }
 
-function dotProduct(v, w){
-    return v[0] * w[0] + v[1] * w[1] + v[2] * w[2]; 
+
+
+for(var x = -canvas.width/2; x < canvas.width/2; x++){
+    for(var y = -canvas.height/2; y < canvas.height/2; y++){
+        var direction = canvasToViewport(x, y);
+        var color = TraceRay(O, direction, 1, Infinity); 
+        
+        PutPixel(x, y, color); 
+    }
 }
 
-function subtract(v1, v2){
-    return [v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2]]; 
-}
+UpdateCanvas(); 
